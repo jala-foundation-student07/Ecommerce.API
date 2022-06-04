f74cff9e4e7cc6b73fdf1fff9866b9f54051d285
Niwer <erwient@hotmail.com>
2022-06-04 10:48:35 -0400

    [EF] Added Models Cart,Product and User for Models layer

---
+++ Ecommerce.Models/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Ecommerce.Models/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+
+namespace Ecommerce.Models.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int IdCart { get; set; }
+        public Cart Cart { get; set; }
+    }
+}
