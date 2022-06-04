0accd7a95445320efd1e58e880d36c10c8d7d640
Niwer <erwient@hotmail.com>
2022-06-04 12:51:04 -0400

    [EF-03] Added CommonProfile mapper

---
+++ Ecommerce.API/Mappings/CommonProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ecommerce.API.ModelsDto;
+using Ecommerce.Models.Entities;
+
+namespace Ecommerce.API.Mappings
+{
+    public class CommonProfile : Profile
+    {
+        public CommonProfile()
+        {
+            CreateMap<Product, ProductDto>();
+            CreateMap<ProductDto, Product>();
+        }
+    }
+}
