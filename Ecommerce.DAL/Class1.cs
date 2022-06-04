1036b95736deb5ea0d47a4c1a493612752797ade
Niwer <erwient@hotmail.com>
2022-06-04 10:09:39 -0400

    [EF]-Added-Files

---
+++ Ecommerce.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ecommerce.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Ecommerce.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ecommerce.Models
+{
+    public class Class1
+    {
+    }
+}
