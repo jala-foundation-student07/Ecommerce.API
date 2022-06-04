aab67c5f4b4316284d5ee9fc74338b0749f9d64c
Niwer <erwient@hotmail.com>
2022-06-04 12:54:34 -0400

    [EF-06] Added Services Project

---
+++ Ecommerce.Service/IProductService.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Service
+{
+    public interface IProductService
+    {
+        /// <summary>
+        /// Return the product find by name.
+        /// </summary>
+        /// <param name="nameProduct">Product identifier</param>
+        /// <returns></returns>
+        Task<Product> GetProductByNameAsync(string nameProduct);
+
+        /// <summary>
+        /// Return all the products.
+        /// </summary>
+        /// <returns>Products</returns>
+        Task<IList<Product>> GetAllProductsAsync();
+    }
+}
